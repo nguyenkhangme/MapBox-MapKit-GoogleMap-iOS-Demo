649dfaa86992576e353fd33926176589b845c5ca
Duncan Nguyen <duncannguyen2k@gmail.com>
2020-04-17 17:18:00 +0700

    add query to API link

--- getDataAPI.swift
+++ getDataAPI.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct abcdef {
-    func loadData() {
-        guard let url = URL(string:"https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=YOUR_MAPBOX_ACCESS_TOKEN") else {
+    func loadData(query: String) {
+        guard let url = URL(string:"https://api.mapbox.com/geocoding/v5/mapbox.places/" + query + ".json?access_token=YOUR_MAPBOX_ACCESS_TOKEN") else {
             print("Invalid URL")
             return
         }
