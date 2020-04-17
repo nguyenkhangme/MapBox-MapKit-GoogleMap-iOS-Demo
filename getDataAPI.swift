b7bf206a9a0e1f1abdcbf3c9c70c6d9a260c0f57
Duncan Nguyen <duncannguyen2k@gmail.com>
2020-04-17 17:28:10 +0700

    Done Step 3 of loadData, one step more: Handle the result of that networking task.

--- getDataAPI.swift
+++ getDataAPI.swift
@@ -7,11 +7,19 @@
 
 import Foundation
 
+//https://www.hackingwithswift.com/books/ios-swiftui/sending-and-receiving-codable-data-with-urlsession-and-swiftui
+
 struct FetchData {
     func loadData(query: String) {
         guard let url = URL(string:"https://api.mapbox.com/geocoding/v5/mapbox.places/" + query + ".json?access_token=YOUR_MAPBOX_ACCESS_TOKEN") else {
             print("Invalid URL")
             return
         }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // Handle the result of that networking task.
+        }.resume()
     }
 }
