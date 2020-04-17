5a17e611215e8e1ec3c788934512c42d01a7a669
Duncan Nguyen <duncannguyen2k@gmail.com>
2020-04-17 08:59:04 +0700

    Initial Commit

---
+++ mapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapApp
+//
+//  Created by Vinova on 4/17/20.
+//  Copyright © 2020 Vinova.Train.mapApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
