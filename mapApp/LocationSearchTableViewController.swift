ab893ff14c38e9d075eb35e39ae31c197fa6cb6c
Duncan Nguyen <duncannguyen2k@gmail.com>
2020-04-22 11:57:52 +0700

    async

--- mapApp/LocationSearchTableViewController.swift
+++ mapApp/LocationSearchTableViewController.swift
@@ -122,30 +122,53 @@ extension LocationSearchTableViewController : UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         
         
-        let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
-    
         
-        options.allowedISOCountryCodes = ["CA"]
-        //specific, near//options.focalLocation = CLLocation(latitude: 45.3, longitude: -66.1)
-        options.allowedScopes = [.address, .pointOfInterest]
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+        
+            let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
+            
+            options.allowedISOCountryCodes = ["CA"]
+            //specific, near//options.focalLocation = CLLocation(latitude: 45.3, longitude: -66.1)
+            options.allowedScopes = [.address, .pointOfInterest]
 
-        _ = self.geocoder.geocode(options) { (placemarks, attribution, error) in
-//            guard let placemark = placemarks?.first else {
+            _ = self!.geocoder.geocode(options) { (placemarks, attribution, error) in
+               
+                if let placemarksX = placemarks {
+                    DispatchQueue.main.async {
+                        //if let placemarksC = placemarks
+                        self!.matchingItems = placemarksX
+                        self!.tableView.reloadData()
+                    }
+                                
+                } else {
+                    return
+                }
+            }
+        
+//        let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
+//
+//
+//        options.allowedISOCountryCodes = ["CA"]
+//        //specific, near//options.focalLocation = CLLocation(latitude: 45.3, longitude: -66.1)
+//        options.allowedScopes = [.address, .pointOfInterest]
+//
+//        _ = self.geocoder.geocode(options) { (placemarks, attribution, error) in
+////            guard let placemark = placemarks?.first else {
+////                return
+////            }
+////
+////            self.matchingItems1.append(placemark)
+//            if let placemarksX = placemarks {
+//
+//                self.matchingItems = placemarksX
+//                self.tableView.reloadData()
+//
+//            } else {
 //                return
 //            }
-//
-//            self.matchingItems1.append(placemark)
-            if let placemarksX = placemarks {
-                
-                self.matchingItems = placemarksX
-                self.tableView.reloadData()
-                
-            } else {
-                return
-            }
             
             
-            print("placemark: \(String(describing: placemarks)),\nmatching: \(self.matchingItems)")
+            //print("placemark: \(String(describing: placemarks)),\nmatching: \(self.matchingItems)")
         }
         
     }
