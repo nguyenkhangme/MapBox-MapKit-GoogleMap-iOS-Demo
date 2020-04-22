bca50d79c3ffc380432320d3fa94a817fa27f8c4
Duncan Nguyen <duncannguyen2k@gmail.com>
2020-04-22 12:53:36 +0700

    clear code

--- mapApp/LocationSearchTableViewController.swift
+++ mapApp/LocationSearchTableViewController.swift
@@ -121,10 +121,8 @@ class LocationSearchTableViewController: UITableViewController {
 extension LocationSearchTableViewController : UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         
-        
-        
-        
-            let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
+        let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
+
 
         DispatchQueue.global(qos: .userInteractive).async { [weak self] in
             
@@ -136,7 +134,9 @@ extension LocationSearchTableViewController : UISearchResultsUpdating {
                
                 if let placemarksX = placemarks {
                     DispatchQueue.main.async {
-                        //if let placemarksC = placemarks
+//                        if options == ForwardGeocodeOptions(query: searchController.searchBar.text ?? "") {
+//
+//                        }
                         self!.matchingItems = placemarksX
                         self!.tableView.reloadData()
                     }
@@ -145,29 +145,6 @@ extension LocationSearchTableViewController : UISearchResultsUpdating {
                     return
                 }
             }
-        
-//        let options = ForwardGeocodeOptions(query: searchController.searchBar.text ?? "")
-//
-//
-//        options.allowedISOCountryCodes = ["CA"]
-//        //specific, near//options.focalLocation = CLLocation(latitude: 45.3, longitude: -66.1)
-//        options.allowedScopes = [.address, .pointOfInterest]
-//
-//        _ = self.geocoder.geocode(options) { (placemarks, attribution, error) in
-////            guard let placemark = placemarks?.first else {
-////                return
-////            }
-////
-////            self.matchingItems1.append(placemark)
-//            if let placemarksX = placemarks {
-//
-//                self.matchingItems = placemarksX
-//                self.tableView.reloadData()
-//
-//            } else {
-//                return
-//            }
-            
             
             //print("placemark: \(String(describing: placemarks)),\nmatching: \(self.matchingItems)")
         }
